6386dccc160432a0cae9504101d0508a6e67f287
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-04 15:17:47 +0200

    [REFACT] SessionAccessInMemory fix

--- server/data-in-memory/session-data/SessionAccessInMemory.ts
+++ server/data-in-memory/session-data/SessionAccessInMemory.ts
@@ -1,5 +1,9 @@
 import SessionGateway from "../../data-gateway/SessionGateway";
 import Session from "../../entities/session/Session";
+import { ActionType } from "../../exceptions/ActionTypes";
+import ActionFailedException from "../../exceptions/ActionFailedException";
+import DoesNotExistException from "../../exceptions/DoesNotExistException";
+import IdDuplicateException from "../../exceptions/IdDuplicateException";
 
 export default class SessionAccessInMemory implements SessionGateway {
   private memory: Session[];
@@ -8,53 +12,28 @@ export default class SessionAccessInMemory implements SessionGateway {
     this.memory = [];
   }
 
-  trySave(session: Session): void {
-    try {
-      this.save(session);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  tryDelete(id: string): void {
-    try {
-      this.delete(id);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  tryFetchAll(): Session[] {
-    try {
-      return this.fetchAll();
-    } catch (e) {
-      console.log(e);
-      const s: Session[] = [];
-      return s;
-    }
-  }
-
-  generateSessionId(date: number): string {
-    return String(
-      date.toString() + "x" + Math.floor(Math.random() * 1000000).toString()
-    );
-  }
-
-  private save(session: Session): void {
+  save(session: Session): void {
     this.checkIfSessionIsDuplicate(session);
     this.memory.push(session);
     this.checkIfSessionSaved(session);
   }
 
-  private delete(id: string): void {
+  delete(id: string): void {
     this.checkIfSessionExists(id);
     this.memory = this.memory.filter((s) => s.getId() !== id);
     this.checkIfSessionDeleted(id);
   }
 
-  private fetchAll(): Session[] {
-    this.checkIfMemoryEmpty();
+  fetchAll(): Session[] {
     return this.memory;
+  }
+
+  generateSessionId(timestamp: number): string {
+    return String(
+      timestamp.toString() +
+        "x" +
+        Math.floor(Math.random() * 1000000).toString()
+    );
   }
 
   private findById(id: string): Session | undefined {
@@ -63,26 +42,19 @@ export default class SessionAccessInMemory implements SessionGateway {
 
   private checkIfSessionSaved(session: Session): void {
     if (!this.memory.includes(session))
-      throw new Error("Could not save to memory");
+      throw new ActionFailedException(session.getId(), ActionType.Save);
   }
   private checkIfSessionIsDuplicate(session: Session): void {
     if (this.findById(session.getId()))
-      throw new Error("The session already exists in memory");
+      throw new IdDuplicateException(session.getId());
   }
 
   private checkIfSessionExists(id: string): void {
-    if (typeof this.findById(id) === undefined)
-      throw new Error("The session does not exist in memory");
+    if (this.findById(id) === undefined) throw new DoesNotExistException(id);
   }
 
   private checkIfSessionDeleted(id: string): void {
-    if (typeof this.findById(id) !== undefined)
-      throw new Error("Could not delete the session from memory");
-  }
-
-  private checkIfMemoryEmpty(): void {
-    if (this.memory.length === 0) {
-      throw new Error("There are no sessions in memory");
-    }
+    if (this.findById(id))
+      throw new ActionFailedException(id, ActionType.Delete);
   }
 }
