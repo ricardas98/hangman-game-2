3ae650d5006d4549152b6b9eed027af944b439a6
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-07 14:11:34 +0200

    [REFACT] ternary

--- server/entities/game/Game.ts
+++ server/entities/game/Game.ts
@@ -26,8 +26,9 @@ export default class Game {
   guess(letter: string): Game {
     if (this.isLetterAlreadyGuessed(letter)) return this;
 
-    if (this.isGuessCorrect(letter)) return this.addMatch(letter);
-    else return this.addMiss(letter);
+    return this.isGuessCorrect(letter)
+      ? this.addMatch(letter)
+      : this.addMiss(letter);
   }
 
   getWord(): string {
@@ -93,8 +94,7 @@ export default class Game {
 
   private selectState(): GameState {
     if (this.isGameLost()) return GameState.Lost;
-    if (this.isGameWon()) return GameState.Won;
-    return GameState.Running;
+    return this.isGameWon() ? GameState.Won : GameState.Running;
   }
 
   private isGameLost(): boolean {
