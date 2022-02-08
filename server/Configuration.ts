97a62f551b1ce1395a8c4d43c661aa47b60bc42d
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-08 15:25:51 +0200

    [FEAT] build update session interactor

--- server/Configuration.ts
+++ server/Configuration.ts
@@ -1,6 +1,7 @@
 import SessionAccessInMemory from "./data-in-memory/session-data/SessionAccessInMemory";
 import WordAccessInMemory from "./data-in-memory/word-data/WordAccessInMemory";
 import CreateSessionInteractor from "./use-cases/implementation/create-session/CreateSessionInteractor";
+import UpdateSessionInteractor from "./use-cases/implementation/update-session/UpdateSessionInteractor";
 
 const sessionGw = new SessionAccessInMemory();
 const wordGw = new WordAccessInMemory();
@@ -9,6 +10,8 @@ export const createSessionInteractor = new CreateSessionInteractor(
   sessionGw,
   wordGw
 );
+
+export const updateSessionInteractor = new UpdateSessionInteractor(sessionGw);
 
 wordGw.save("parrot");
 wordGw.save("hamster");
