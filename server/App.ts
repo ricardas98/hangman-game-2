54b249702913aee27e04fd322e631e927d891724
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-08 15:23:14 +0200

    [FEAT] express.json

--- server/App.ts
+++ server/App.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./routes/SessionRoutes";
 const app = express();
 
+app.use(express.json());
 app.use("/api/sessions", router);
 
 export default app;
