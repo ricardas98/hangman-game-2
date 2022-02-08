8257cf5de4481dfc21857019a9a1ebdca96f6c14
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-08 11:42:11 +0200

    [REFACT] changed implementation to interface

--- server/controllers/rest/SessionControllerREST.test.ts
+++ server/controllers/rest/SessionControllerREST.test.ts
@@ -1,18 +1,17 @@
 import { mock } from "jest-mock-extended";
-import Session from "../../entities/session/Session";
-import CreateSessionInteractor from "../../use-cases/implementation/create-session/CreateSessionInteractor";
 import SessionControllerREST from "./SessionControllerREST";
 import SessionOutputData from "../../output-data/SessionOutputData";
 import { GameState } from "../../entities/game-state/GameState";
 import SessionB2RConverter from "../../presenters/rest/SessionB2RConverter";
-import { getMockReq, getMockRes } from "@jest-mock/express";
+import { getMockRes } from "@jest-mock/express";
+import CreateSessionsUseCase from "../../use-cases/input-boundary-models/CreateSessionUseCase";
 
 describe("Session controller", () => {
   let controller: SessionControllerREST;
   let outputData: SessionOutputData;
 
   function mockInteractor() {
-    const interactor = mock<CreateSessionInteractor>();
+    const interactor = mock<CreateSessionsUseCase>();
     interactor.create.mockReturnValue(
       new SessionOutputData("1", GameState.Running, ["a"], ["b", "c"])
     );
