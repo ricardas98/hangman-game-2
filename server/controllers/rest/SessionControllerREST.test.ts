2bba0470e29a3069889bca24a55f5aa9af7d7549
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-08 09:15:20 +0200

    [FEAT] progress on controller test

--- server/controllers/rest/SessionControllerREST.test.ts
+++ server/controllers/rest/SessionControllerREST.test.ts
@@ -1,12 +1,28 @@
-import { mock } from "jest-mock-extended";
+import Session from "../../entities/session/Session";
 import CreateSessionInteractor from "../../use-cases/implementation/create-session/CreateSessionInteractor";
 import SessionControllerREST from "./SessionControllerREST";
 
+jest.mock(
+  "../../use-cases/implementation/create-session/CreateSessionInteractor",
+  () => {
+    return {
+      CreateSessionInteractor: jest.fn().mockImplementation(() => {
+        return {
+          create: () => {
+            return new Session("1", 134446, "cat");
+          },
+        };
+      }),
+    };
+  }
+);
+
 describe("Session controller", () => {
   let controller: SessionControllerREST;
+  let interactor;
 
   function createController() {
-    controller = new SessionControllerREST(mock<CreateSessionInteractor>());
+    controller = new SessionControllerREST(new CreateSessionInteractor());
   }
 
   beforeAll(() => {
@@ -16,4 +32,6 @@ describe("Session controller", () => {
   it("is created", () => {
     expect(controller).toBeDefined();
   });
+
+  it("creates session", () => {});
 });
