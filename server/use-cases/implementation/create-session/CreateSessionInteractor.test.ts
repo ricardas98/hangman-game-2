a5d43a3bf28ff350bd1879dc59be7f2175f3683a
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-08 11:37:45 +0200

    [REFACT] mocked gateways

--- server/use-cases/implementation/create-session/CreateSessionInteractor.test.ts
+++ server/use-cases/implementation/create-session/CreateSessionInteractor.test.ts
@@ -4,18 +4,33 @@ import { GameState } from "../../../entities/game-state/GameState";
 import OutputData from "../../../output-data/SessionOutputData";
 import CreateGameUseCase from "../../input-boundary-models/CreateSessionUseCase";
 import CreateSessionInteractor from "./CreateSessionInteractor";
+import { MockProxy, mock } from "jest-mock-extended";
+import SessionGateway from "../../../data-gateway/SessionGateway";
+import WordGateway from "../../../data-gateway/WordGateway";
 
 describe("Create session interactor", () => {
   let interactor: CreateGameUseCase;
+  let sessionAccessInMemory: MockProxy<SessionGateway>;
+  let wordAccessInMemory: MockProxy<WordGateway>;
+
+  function buildMockSessionAccessInMemory() {
+    sessionAccessInMemory = mock<SessionGateway>();
+  }
+
+  function buildMockWordAccessInMemory() {
+    wordAccessInMemory = mock<WordGateway>();
+  }
 
   function initInteractor() {
     interactor = new CreateSessionInteractor(
-      new SessionAccessInMemory(),
-      new WordAccessInMemory()
+      sessionAccessInMemory,
+      wordAccessInMemory
     );
   }
 
   beforeEach(() => {
+    buildMockSessionAccessInMemory();
+    buildMockWordAccessInMemory();
     initInteractor();
   });
 
@@ -24,9 +39,12 @@ describe("Create session interactor", () => {
   });
 
   it("creates game", () => {
+    sessionAccessInMemory.generateSessionId.mockReturnValue("1");
+    sessionAccessInMemory.save.mockImplementation(() => {});
+    wordAccessInMemory.getRandomWord.mockReturnValue("cat");
     const res: OutputData = interactor.create();
 
-    expect(res.getSessionId()).toBeDefined();
+    expect(res.getSessionId()).toBe("1");
     expect(res.getGameState()).toBe(GameState.Running);
     expect(res.getMatches()).toEqual([]);
     expect(res.getMisses()).toEqual([]);
