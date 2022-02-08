7fd4e4bdbbc400bf1e76de6bcc013238d98d99c8
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-08 15:25:30 +0200

    [FEAT] update route

--- server/routes/SessionRoutes.ts
+++ server/routes/SessionRoutes.ts
@@ -1,13 +1,26 @@
-import { createSessionInteractor } from "../Configuration";
+import {
+  createSessionInteractor,
+  updateSessionInteractor,
+} from "../Configuration";
 import SessionCreateControllerREST from "../controllers/rest/create/SessionCreateControllerREST";
 import SessionB2RConverter from "../presenters/rest/SessionB2RConverter";
+import { Request, Response } from "express";
+import SessionUpdateControllerREST from "../controllers/rest/update/SessionUpdateControllerREST";
+
 const router = require("express").Router();
 
-router.post("/", (req: any, res: any) => {
+router.post("/", (req: Request, res: Response) => {
   new SessionCreateControllerREST(
     createSessionInteractor,
     new SessionB2RConverter()
   ).create(res);
+});
+
+router.put("/:id", (req: Request, res: Response) => {
+  new SessionUpdateControllerREST(
+    updateSessionInteractor,
+    new SessionB2RConverter()
+  ).update(req, res);
 });
 
 export default router;
