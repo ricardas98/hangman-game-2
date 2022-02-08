4d22c7bc45443b8ebd107cf8f60b14be9dfcf53a
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-08 11:04:30 +0200

    [FEAT] implemented create session use case

--- server/controllers/rest/SessionControllerREST.test.ts
+++ server/controllers/rest/SessionControllerREST.test.ts
@@ -1,7 +1,11 @@
-import { createSessionInteractor } from "../../Configuration";
+import { mock } from "jest-mock-extended";
 import Session from "../../entities/session/Session";
 import CreateSessionInteractor from "../../use-cases/implementation/create-session/CreateSessionInteractor";
 import SessionControllerREST from "./SessionControllerREST";
+import SessionOutputData from "../../output-data/SessionOutputData";
+import { GameState } from "../../entities/game-state/GameState";
+import DataConverterREST from "../../presenters/rest/DataConverterREST";
+import { getMockReq, getMockRes } from "@jest-mock/express";
 
 jest.mock(
   "../../use-cases/implementation/create-session/CreateSessionInteractor",
@@ -20,13 +24,31 @@ jest.mock(
 
 describe("Session controller", () => {
   let controller: SessionControllerREST;
-  let interactor;
+  let outputData: SessionOutputData;
 
-  function createController() {
-    controller = new SessionControllerREST(createSessionInteractor());
+  function mockInteractor() {
+    const interactor = mock<CreateSessionInteractor>();
+    interactor.create.mockReturnValue(
+      new SessionOutputData("1", GameState.Running, ["a"], ["b", "c"])
+    );
+    return interactor;
   }
 
-  beforeAll(() => {
+  function createController() {
+    controller = new SessionControllerREST(mockInteractor());
+  }
+
+  function setOutputData() {
+    outputData = new SessionOutputData(
+      "1",
+      GameState.Running,
+      ["a"],
+      ["b", "c"]
+    );
+  }
+
+  beforeEach(() => {
+    setOutputData();
     createController();
   });
 
@@ -34,5 +56,14 @@ describe("Session controller", () => {
     expect(controller).toBeDefined();
   });
 
-  it("creates session", () => {});
+  it("created a new session", async () => {
+    const { res } = getMockRes();
+
+    await controller.create(res);
+
+    expect(res.status).toBeCalledWith(201);
+    expect(res.send).toBeCalledWith(
+      new DataConverterREST().processData(outputData)
+    );
+  });
 });
--- server/controllers/rest/SessionControllerREST.ts
+++ server/controllers/rest/SessionControllerREST.ts
@@ -1,18 +1,20 @@
 import SessionInputData from "../../input-data/SessionInputData";
 import OutputData from "../../output-data/SessionOutputData";
 import DataConverterREST from "../../presenters/rest/DataConverterREST";
+import CreateSessionInteractor from "../../use-cases/implementation/create-session/CreateSessionInteractor";
 import CreateSessionsUseCase from "../../use-cases/input-boundary-models/CreateSessionUseCase";
+import { Response } from "express";
 
 export default class SessionControllerREST {
   private createSessionsUC: CreateSessionsUseCase;
   private presenter: DataConverterREST;
 
-  constructor(createSessionsUC: CreateSessionsUseCase) {
-    this.createSessionsUC = createSessionsUC;
+  constructor(createSessionCallBack: CreateSessionInteractor) {
+    this.createSessionsUC = createSessionCallBack;
     this.presenter = new DataConverterREST();
   }
 
-  create(res: any): void {
+  create(res: Response): void {
     const sessionData = this.createSessionsUC.create();
     res.status(201).send(this.presenter.processData(sessionData));
   }
