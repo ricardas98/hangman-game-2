831098a572e640051d09dde869ed1f34d4799610
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-08 12:11:38 +0200

    [REFACT] removed unused test

--- server/routes/SessionRoutes.ts
+++ server/routes/SessionRoutes.ts
@@ -3,10 +3,6 @@ import SessionControllerREST from "../controllers/rest/SessionControllerREST";
 import DataConverterREST from "../presenters/rest/SessionB2RConverter";
 const router = require("express").Router();
 
-router.get("/", (req: any, res: any) => {
-  res.status(200).json({ status: "working" });
-});
-
 router.post("/", (req: any, res: any) => {
   new SessionControllerREST(
     createSessionInteractor,
