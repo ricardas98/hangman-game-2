365badff704e651b9d0c175270859718d88b4e71
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-09 09:36:30 +0200

    [FEAT] delete session test

--- server/App.test.ts
+++ server/App.test.ts
@@ -2,6 +2,16 @@ import app from "./App";
 const request = require("supertest");
 
 describe("App", () => {
+  let game: any;
+
+  async function createGame() {
+    game = await request(app).post("/api/sessions").send();
+  }
+
+  beforeEach(() => {
+    createGame();
+  });
+
   it("creates a new session", async () => {
     const res = await request(app).post("/api/sessions").send();
 
@@ -13,10 +23,9 @@ describe("App", () => {
   });
 
   it("updates session", async () => {
-    const game = await request(app).post("/api/sessions").send();
     const res = await request(app)
       .put(`/api/sessions/${game.body.id}`)
-      .send({ guess: "a" }));
+      .send({ guess: "a" });
 
     expect(res.statusCode).toBe(200);
     expect(res.body.id).toBeDefined();
@@ -24,5 +33,13 @@ describe("App", () => {
     expect(res.body.matches).toBeDefined();
     expect(res.body.misses).toBeDefined();
     expect(res.body.matches + res.body.misses).toEqual("a");
+  });
+
+  it("deletes session", async () => {
+    const res = await request(app)
+      .delete(`/api/sessions/${game.body.id}`)
+      .send();
+
+    expect(res.statusCode).toBe(204);
   });
 });
