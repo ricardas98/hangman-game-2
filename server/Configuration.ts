42c1d535d8698bb7d3dbfad8231f636c3bccdaaf
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-09 09:36:55 +0200

    [FEAT] exports prebuilt deleteSessionInteractor

--- server/Configuration.ts
+++ server/Configuration.ts
@@ -1,10 +1,17 @@
 import SessionAccessInMemory from "./data-in-memory/session-data/SessionAccessInMemory";
 import WordAccessInMemory from "./data-in-memory/word-data/WordAccessInMemory";
 import CreateSessionInteractor from "./use-cases/implementation/create-session/CreateSessionInteractor";
+import DeleteSessionInteractor from "./use-cases/implementation/delete-session/DeleteSessionInteractor";
 import UpdateSessionInteractor from "./use-cases/implementation/update-session/UpdateSessionInteractor";
 
 const sessionGw = new SessionAccessInMemory();
 const wordGw = new WordAccessInMemory();
+
+wordGw.save("parrot");
+wordGw.save("hamster");
+wordGw.save("tiger");
+wordGw.save("koala");
+wordGw.save("monkey");
 
 export const createSessionInteractor = new CreateSessionInteractor(
   sessionGw,
@@ -13,8 +20,4 @@ export const createSessionInteractor = new CreateSessionInteractor(
 
 export const updateSessionInteractor = new UpdateSessionInteractor(sessionGw);
 
-wordGw.save("parrot");
-wordGw.save("hamster");
-wordGw.save("tiger");
-wordGw.save("koala");
-wordGw.save("monkey");
+export const deleteSessionInteractor = new DeleteSessionInteractor(sessionGw);
