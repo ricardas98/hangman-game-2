b525d165ad28934a47b03476ef50660bd2787168
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-10 11:26:26 +0200

    [FEAT] converter returns object instead of string

--- server/presenters/rest/SessionB2RConverter.ts
+++ server/presenters/rest/SessionB2RConverter.ts
@@ -1,12 +1,12 @@
 import BoundarySessionOutput from "../../output-data/BoundarySessionOutput";
 
 export default class DataConverterREST {
-  processData(data: BoundarySessionOutput): string {
-    return JSON.stringify({
+  processData(data: BoundarySessionOutput): object {
+    return {
       id: data.getSessionId(),
       state: data.getGameState(),
       matches: data.getMatches(),
       misses: data.getMisses(),
-    });
+    };
   }
 }
