0289a4cb726b415fd16aac66118d3ce89860d791
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-10 12:06:42 +0200

    [FEAT] created RestSession data structure

--- server/presenters/rest/SessionB2RConverter.ts
+++ server/presenters/rest/SessionB2RConverter.ts
@@ -1,12 +1,13 @@
 import BoundarySessionOutput from "../../output-data/BoundarySessionOutput";
+import RestSession from "./RestSesion";
 
-export default class DataConverterREST {
-  processData(data: BoundarySessionOutput): object {
-    return {
-      id: data.getSessionId(),
-      state: data.getGameState(),
-      matches: data.getMatches(),
-      misses: data.getMisses(),
-    };
+export default class SessionB2RConverter {
+  processData(data: BoundarySessionOutput): RestSession {
+    return new RestSession(
+      data.getSessionId(),
+      data.getGameState(),
+      data.getMatches(),
+      data.getMisses()
+    );
   }
 }
