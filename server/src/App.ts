daa30982fb4bbdfab064f23030cf1e805e358ab1
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-14 12:13:27 +0200

    [FEAT] use cors

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 export default class App {
   private app: any;
@@ -7,6 +8,7 @@ export default class App {
   constructor(router: any, port: number) {
     this.port = port;
     this.app = express();
+    this.app.use(cors());
     this.app.use(express.json());
     this.app.use("/api/sessions", router);
   }
