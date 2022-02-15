d4f506dc0b72332990fd7eaa2ccf01c36d824c8d
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-15 14:25:09 +0200

    [FEAT] home page

---
+++ client/src/view/container/homeWindow/HomePage.tsx
@@ -0,0 +1,14 @@
+import { createSessionController } from "../../../Configuration";
+import { useHomePage } from "./useHomePage"
+
+export const HomePage = () => {
+
+    const session = useHomePage(createSessionController)
+
+    return(
+    <div>
+        <h1>Hangman game</h1>
+        <div>ID: {session.id}</div>
+    </div>
+    );
+}
