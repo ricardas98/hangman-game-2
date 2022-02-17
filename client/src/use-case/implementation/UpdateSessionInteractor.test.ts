de7bf1af6794a7b791d2734df08aaa1b3b8659f4
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-17 15:16:21 +0200

    [FEAT] update session interactor

---
+++ client/src/use-case/implementation/UpdateSessionInteractor.test.ts
@@ -0,0 +1,46 @@
+import { Session } from "../../domain/Session";
+import { SessionGateway } from "../../gateway/api/SessionGateway";
+import { MockProxy, mock } from "jest-mock-extended";
+import { UpdateSessionUseCase } from "../../use-case/api/UpdateSessionUseCase";
+import { UpdateSessionInteractor } from "./UpdateSessionInteractor";
+import { SessionD2BConverter } from "./converter/SessionD2BConverter";
+import { of } from "rxjs";
+import { BoundarySessionOutput } from "../../use-case/api/entity/BoundarySessionOutput";
+
+describe("Update session interactor", () => {
+  let interactor: UpdateSessionUseCase;
+  let gateway: MockProxy<SessionGateway>;
+
+  beforeEach(() => {
+    mockGateway();
+    initInteractor();
+  });
+
+  it("updates game", done => {
+    const session: Session = new Session("123", 0, ["a"], [], [[0, "a"]]);
+    const boundarySession = new BoundarySessionOutput(
+      "123",
+      0,
+      ["a"],
+      [],
+      [[0, "a"]]
+    );
+    gateway.create.mockReturnValue(of(session));
+
+    interactor.update("123", "a").subscribe(s => {
+      expect(s).toEqual(boundarySession);
+      done();
+    });
+  });
+
+  function mockGateway() {
+    gateway = mock<SessionGateway>();
+  }
+
+  function initInteractor() {
+    interactor = new UpdateSessionInteractor(
+      gateway,
+      new SessionD2BConverter()
+    );
+  }
+});
---
+++ client/src/use-case/implementation/UpdateSessionInteractor.ts
@@ -0,0 +1,22 @@
+import { BoundarySessionOutput } from "use-case/api/entity/BoundarySessionOutput";
+import { UpdateSessionUseCase } from "use-case/api/UpdateSessionUseCase";
+import { Observable } from "rxjs";
+import { SessionGateway } from "gateway/api/SessionGateway";
+import { SessionD2BConverter } from "./converter/SessionD2BConverter";
+import { map } from "rxjs/operators";
+
+export class UpdateSessionInteractor implements UpdateSessionUseCase {
+  private readonly gateway: SessionGateway;
+  private readonly converter: SessionD2BConverter;
+
+  constructor(gateway: SessionGateway, converter: SessionD2BConverter) {
+    this.gateway = gateway;
+    this.converter = converter;
+  }
+
+  update(id: string, guess: string) {
+    return this.gateway
+      .update(id, guess)
+      .pipe(map(s => this.converter.processData(s)));
+  }
+}
