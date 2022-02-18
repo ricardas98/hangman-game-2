c41ee912cc261c19b7f88aa84ab6313ebd8ebd5e
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-18 14:15:50 +0200

    [REFACT] changed setSession type

--- client/src/view/container/gameWindow/useGameWindow.ts
+++ client/src/view/container/gameWindow/useGameWindow.ts
@@ -3,7 +3,7 @@ import { ViewSession } from "controller/model/ViewSession";
 
 export function useGameWindow(
   controller: UpdateSessionController,
-  setSession: React.Dispatch<React.SetStateAction<ViewSession | undefined>>
+  setSession: (session: ViewSession | undefined) => void
 ): (id: string, letter: string) => void {
   const updateGame = (id: string, letter: string) => {
     controller.update(id, letter).subscribe(setSession);
