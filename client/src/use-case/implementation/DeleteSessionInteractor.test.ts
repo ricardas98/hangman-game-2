7f562d7034dcead4d4bca8591d9266dee5f3b88a
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-18 11:07:35 +0200

    [FEAT] delete session use case

---
+++ client/src/use-case/implementation/DeleteSessionInteractor.test.ts
@@ -0,0 +1,32 @@
+import { SessionGateway } from "gateway/api/SessionGateway";
+import { mock, MockProxy } from "jest-mock-extended";
+import { of } from "rxjs";
+import { SessionD2BConverter } from "./converter/SessionD2BConverter";
+import { DeleteSessionInteractor } from "./DeleteSessionInteractor";
+
+describe("Delete session interactor", () => {
+  let interactor: DeleteSessionInteractor;
+  let gateway: MockProxy<SessionGateway>;
+
+  beforeEach(() => {
+    mockGateway();
+    initInteractor();
+  });
+
+  it("deletes session", () => {
+    gateway.delete.mockReturnValue(of(204));
+
+    interactor.delete("1").subscribe(res => expect(res).toBeTruthy());
+  });
+
+  function mockGateway() {
+    gateway = mock<SessionGateway>();
+  }
+
+  function initInteractor() {
+    interactor = new DeleteSessionInteractor(
+      gateway,
+      new SessionD2BConverter()
+    );
+  }
+});
