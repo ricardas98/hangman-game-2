ff259890721f6355182ecd9f373994b6c76b4578
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-22 12:33:35 +0200

    [FEAT] footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@mui/material";
 import { theme } from "MaterialUiTheme";
+import { Footer } from "view/component/Footer";
 import { MainWindow } from "./view/container/main-window/MainWindow";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <MainWindow />
+      <Footer />
     </ThemeProvider>
   );
 }
---
+++ client/src/view/component/Footer.tsx
@@ -0,0 +1,35 @@
+import { Box, Button, Grid, Link, Typography } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+export const Footer = () => {
+  return (
+    <Box
+      px={4}
+      mt={4}
+      sx={{
+        minHeight: "150px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "secondary.main",
+      }}
+    >
+      <Box width="960px">
+        <Grid container justifyContent="space-between" alignItems="flex-start">
+          <Grid item>
+            <Typography color="secondary.light">Hangman Game</Typography>
+            <Typography color="secondary.light">Ricardas, 2022</Typography>
+          </Grid>
+          <Grid item>
+            <Link
+              href="https://github.com/ricardas98/hangman-game-2"
+              target="_blank"
+            >
+              <GitHubIcon color="primary" />
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
