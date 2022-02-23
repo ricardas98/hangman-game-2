0ab32249238d660018328023e175e6a4ae64727b
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-23 12:34:05 +0200

    [REFACT] delete session window

--- client/src/view/container/session-delete-window/DeleteSessionWindow.test.tsx
+++ client/src/view/container/session-delete-window/DeleteSessionWindow.test.tsx
@@ -4,24 +4,40 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { DeleteSessionWindow } from "./DeleteSessionWindow";
-import * as useDeleteSessionWindow from "./useSessionDeleteWindow"
+import * as useDeleteSessionWindow from "./useSessionDeleteWindow";
 
 describe("Delete session window", () => {
-    it("displays button", () => {
-        const setSession = jest.fn();
+  it("displays button", () => {
+    const setSession = jest.fn();
+    const closeModal = jest.fn();
 
-        render(<DeleteSessionWindow id="123" setSession={setSession} />);
+    render(
+      <DeleteSessionWindow
+        id="123"
+        setSession={setSession}
+        closeModal={closeModal}
+      />
+    );
 
-        expect(screen.getByTestId("DeleteButton")).toBeInTheDocument();
-    })
+    expect(screen.getByTestId("DeleteButton")).toBeInTheDocument();
+  });
 
-    it("calls setSession when the button is clicked", () => {
-        const setSession = jest.fn();
-        jest.spyOn(useDeleteSessionWindow, "useDeleteSessionWindow").mockReturnValue(setSession);
-        render(<DeleteSessionWindow id="123" setSession={setSession} />);
+  it("calls setSession when the button is clicked", () => {
+    const setSession = jest.fn();
+    const closeModal = jest.fn();
+    jest
+      .spyOn(useDeleteSessionWindow, "useDeleteSessionWindow")
+      .mockReturnValue(setSession);
+    render(
+      <DeleteSessionWindow
+        id="123"
+        setSession={setSession}
+        closeModal={closeModal}
+      />
+    );
 
-        fireEvent.click(screen.getByTestId("DeleteButton"));
+    fireEvent.click(screen.getByTestId("DeleteButton"));
 
-        expect(setSession).toBeCalled();
-    })
-})
+    expect(setSession).toBeCalled();
+  });
+});
--- client/src/view/container/session-delete-window/DeleteSessionWindow.tsx
+++ client/src/view/container/session-delete-window/DeleteSessionWindow.tsx
@@ -1,15 +1,59 @@
-import { deleteSessionController } from "../../../Configuration"
-import { ViewSession } from "controller/model/ViewSession"
-import { useDeleteSessionWindow } from "./useSessionDeleteWindow"
+import { deleteSessionController } from "../../../Configuration";
+import { ViewSession } from "controller/model/ViewSession";
+import { useDeleteSessionWindow } from "./useSessionDeleteWindow";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { CardWindow } from "view/component/CardWindow";
 
 interface DeleteSessionWindowProps {
-    id: string,
-    setSession: (session: ViewSession | undefined) => void
+  id: string;
+  setSession: (session: ViewSession | undefined) => void;
+  closeModal: () => void;
 }
 
-export const DeleteSessionWindow = ({id, setSession} : DeleteSessionWindowProps) => {
+export const DeleteSessionWindow = ({
+  id,
+  setSession,
+  closeModal,
+}: DeleteSessionWindowProps) => {
+  const deleteSession = useDeleteSessionWindow(
+    deleteSessionController,
+    setSession
+  );
 
-    const deleteSession = useDeleteSessionWindow(deleteSessionController, setSession)
-
-    return <button data-testid="DeleteButton" onClick={() => {deleteSession(id)}}>Quit</button>
-}
+  return (
+    <Box>
+      <Typography variant="h3" mb={4}>
+        Do you really want to quit?
+      </Typography>
+      <Grid
+        container
+        justifyContent={"center"}
+        alignItems={"center"}
+        spacing={2}
+      >
+        <Grid item>
+          <Button
+            data-testid="DeleteButton"
+            onClick={() => closeModal()}
+            variant="contained"
+            color="info"
+          >
+            Resume
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button
+            data-testid="DeleteButton"
+            onClick={() => {
+              deleteSession(id);
+            }}
+            variant="contained"
+            color="error"
+          >
+            Quit
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
