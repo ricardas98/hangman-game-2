788b6d8b9ac5c959468887232f81f9dedc259a3b
Ricardas Serneckas <ricardas98@gmail.com>
2022-02-28 15:01:51 +0200

    [FEAT] error messages based on status code

--- client/src/view/container/observer/useObserver.tsx
+++ client/src/view/container/observer/useObserver.tsx
@@ -1,16 +1,27 @@
 import { useSnackbar } from "notistack";
 import { Observer } from "rxjs";
+import { AjaxError } from "rxjs/ajax";
 
 export function useObserver<T>(callback: (params: T) => void): Observer<T> {
   const { enqueueSnackbar } = useSnackbar();
 
   return {
     next: (params: T) => callback(params),
-    error: () =>
+    error: (err: Error) => handleError(err as AjaxError),
+    complete: () => {},
+  };
+
+  function handleError(err: AjaxError) {
+    if (err.status === 500)
       enqueueSnackbar("Internal server error", {
         preventDuplicate: true,
         variant: "error",
-      }),
-    complete: () => {},
-  };
+      });
+
+    if (err.status === 404)
+      enqueueSnackbar("Session error", {
+        preventDuplicate: true,
+        variant: "error",
+      });
+  }
 }
